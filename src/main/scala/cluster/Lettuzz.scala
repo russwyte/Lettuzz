aebcca4b411b269ce95cc3c5a317075e62228832
Russell White <356303+russwyte@users.noreply.github.com>
2022-09-22 09:05:16 -0500

    don't need that anymore

--- src/main/scala/cluster/Lettuzz.scala
+++ src/main/scala/cluster/Lettuzz.scala
@@ -5,7 +5,7 @@ import io.lettuce.core.cluster.RedisClusterClient
 import io.lettuce.core.codec.{RedisCodec, StringCodec}
 import zio.{Tag, ZIO, ZLayer}
 
-def client[T <: RedisClusterClient](client: => T) =
+def client(client: => RedisClusterClient) =
   ZLayer.scoped { ZIO.attempt(client).withFinalizer(close) }
 
 def commands[K, V](codec: RedisCodec[K, V] = StringCodec.UTF8)(using Tag[K], Tag[V]) =
